95cb41f0f79820f3ca23deaddd13fc6bd54627e3
josh182014 <josh182014@gmail.com>
2019-10-07 16:52:29 -0600

    linted.

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -5,7 +5,8 @@ from util import Stack, Queue  # These may come in handy
 
 
 class Graph:
-    """Represent a graph as a dictionary of vertices mapping labels to edges."""
+    """Represent a graph as a dictionary of vertices
+    mapping labels to edges."""
 
     def __init__(self):
         self.vertices = {}
